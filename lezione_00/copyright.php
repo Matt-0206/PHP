f288ae93225f07d151076832fa862cbc9e2d1d6e
Matteo_Alliu <malliu431@gmail.com>
2024-10-07 12:05:58 +0200

    07/10/2024

---
+++ lezione_00/copyright.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <meta charset = "utf-8">
+
+        <title>Copyrght</title>
+
+    </head>
+
+    <body>
+        
+        <!-- Inserisco l'anno dello sviluppo del sito: date("Y"). "Y" sta per "Year", "echo" stampa sulla pagina le informazioni -->
+        <!-- &copy crea il "Simbolo del copyright" -->
+        <p>Sito sviluppato nel &copy; <?php echo date('Y'); ?></p>
+
+    </body>
+
+</html> 
---
+++ lezione_00/variabili.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <meta charset = "utf-8">
+
+        <title>Copyrght</title>
+
+    </head>
+
+    <body>
+        
+        <?php 
+        
+            $frutta = 'mela'; //Dichiaro una variabile utilizzando $nome_variabile = 'valore' 
+
+            $TotaleScontrino = '125'; //Dichiariamo una seconda variabile
+
+            echo $frutta; //Stampa il valora di frutta sulla pagina
+
+            echo $TotaleScontrino; //Stampo il valore della seconda variabile sulla pagina
+
+        ?>
+
+    </body>
+
+</html> 

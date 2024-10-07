cdc1c0b596a05002c82e6bafffd374955a696ac0
Matteo_Alliu <malliu431@gmail.com>
2024-10-07 11:33:47 +0200

    07/10/2024

---
+++ lezione_00/info.php
@@ -0,0 +1,13 @@
+<!-- Per poter funzionare tutto il codice PHP va inserita all'interno di questa sezione. Se viene aperta in un browser non viene mostrato niente, questo perché
+     PHP è un linguaggio creato e utilizzato per i server. -->
+
+<? php
+
+phpinfo();
+
+?>
+
+<!-- Dove inserire i file:
+ 
+     Aprire Xampp e trovare la voce expolrer
+     Explorer -> htdocs (ATTENZIONE, htdocs contiene tutti i file che verranno resi pubblici e visibili a TUTTI) -> php-8 -> lezione_00 -->

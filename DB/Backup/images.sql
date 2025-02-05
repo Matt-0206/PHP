89a40cb1dd8e5d3c0519e0e774b5296b014fdd54
Matteo_Alliu <malliu431@gmail.com>
2025-02-05 11:28:14 +0100

    05/02/2025

---
+++ DB/Backup/images.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Feb 05, 2025 alle 10:27
+-- Versione del server: 10.4.32-MariaDB
+-- Versione PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpsol`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `images`
+--
+
+DROP TABLE IF EXISTS `images`;
+CREATE TABLE `images` (
+  `image_id` int(10) UNSIGNED NOT NULL,
+  `filename` varchar(25) NOT NULL,
+  `caption` varchar(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dump dei dati per la tabella `images`
+--
+
+INSERT INTO `images` (`image_id`, `filename`, `caption`) VALUES
+(1, 'basin.jpg', 'Water basin at Ryoanji temple, Kyoto'),
+(2, 'fountains.jpg', 'Fountains in central Tokyo'),
+(3, 'kinkakuji.jpg', 'The Golden Pavilion in Kyoto'),
+(4, 'maiko.jpg', 'Maiko&mdash;trainee geishas in Kyoto'),
+(5, 'maiko_phone.jpg', 'Every maiko should have one&mdash;a mobile, of course'),
+(6, 'menu.jpg', 'Menu outside restaurant in Pontocho, Kyoto'),
+(7, 'monk.jpg', 'Monk begging for alms in Kyoto'),
+(8, 'ryoanji.jpg', 'Autumn leaves at Ryoanji temple, Kyoto');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`image_id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `images`
+--
+ALTER TABLE `images`
+  MODIFY `image_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
